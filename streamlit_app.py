f2bf1b65a067ebf5d863d77fdec0046bc49a6200
marcinpytlik <77489303+marcinpytlik@users.noreply.github.com>
2022-09-05 23:00:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
-streamlit.header('Brekfast Menu')
-streamlit.text('Omega 3 & Blueberry Oametal')
-streamlit.text('ollaboga')
-streamlit.text('olaboga2')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
