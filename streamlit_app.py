65e6d0fcc35cec44492d54ef1bc4a48d29252d2e
marcinpytlik <77489303+marcinpytlik@users.noreply.github.com>
2022-09-08 12:31:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 streamlit.title('My Parents new Healthy Diner')
